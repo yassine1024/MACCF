ab82ed73eea03cac19bf398df46a8711553c4ec8
yassine1024 <atik.yassine.1024@gmail.com>
2023-04-17 13:25:08 +0200

    Fix envoyerEmail() in Mail class.
    First add javax.activation-1.2.0.jar;
    Secondly add 'MAIL_USERNAME', 'MAIL_PASSWORD' in configuration.properties and initialize them in Parametrage;
    Finnally add these two parameters to prop instance in envoyerEmail() function.

--- src/launch/Parametrage.java
+++ src/launch/Parametrage.java
@@ -1,4 +1,5 @@
 package launch;
+
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -14,68 +15,69 @@ import java.util.Properties;
 import loaddata.Mail;
 
 public class Parametrage {
-	
-	public static String CorpusPath ;
-	public static String GlobalFiles ;
-	public static String Abside ;
-	public static String BlocMontant ;
-	public static String CompteBloc ;
-	public static String CompteClient ;
-	public static String Operation ;
-	public static String SERVEUR ;
-	public static Integer PORT ;
-	public static String USER ;
-	public static String PASS ;
-	public static String PathClient ;
-	public static String PathCompteBloquer ;
-	public static String PathMontantBloquer ;
-	public static String PathMouvement ;
-	public static String SERVEURSMTP ;
-	public static String FROM ;
-	public static List<String> EmailAdmin ;
-	public static Connection Connection ;
-	
-		
-	
-	public static void Init() throws IOException,SQLException, ClassNotFoundException 
-	{
-		
-			Properties prop = new Properties();
-			FileInputStream in = new FileInputStream("Configuration.properties");
-			prop.load(in);
-			in.close();
-					
-			Parametrage.CorpusPath = prop.getProperty("CorpusPath");
-			Parametrage.GlobalFiles = prop.getProperty("GlobalFiles") ;
-			Parametrage.Abside = prop.getProperty("Abside") ;
-			Parametrage.BlocMontant = prop.getProperty("BlocMontant");
-			Parametrage.CompteBloc = prop.getProperty("CompteBloc") ;
-			Parametrage.CompteClient = prop.getProperty("CompteClient") ;
-			Parametrage.Operation = prop.getProperty("Operation") ;
-			Parametrage.SERVEUR = prop.getProperty("SERVEUR");
-			Parametrage.PORT = Integer.parseInt(prop.getProperty("PORT"));
-			Parametrage.USER = prop.getProperty("USER") ;
-			Parametrage.PASS = prop.getProperty("PASS") ;
-			Parametrage.PathClient = prop.getProperty("PathClient");
-			Parametrage.PathCompteBloquer = prop.getProperty("PathCompteBloquer");
-			Parametrage.PathMontantBloquer = prop.getProperty("PathMontantBloquer");
-			Parametrage.PathMouvement = prop.getProperty("PathMouvement");
-			Parametrage.SERVEURSMTP = prop.getProperty("SERVEURSMTP");
-			Parametrage.FROM = prop.getProperty("FROM");
-			Parametrage.EmailAdmin = new ArrayList<String>();
-			
-			String[] t = prop.getProperty("EmailAdmin").split(",",-1);
-			for(int i=0;i<t.length;i++)
-			{
-				Parametrage.EmailAdmin.add(t[i]) ;
-			}
-			
-			Class.forName("com.mysql.jdbc.Driver");
-						
-			String ChaineConnection = prop.getProperty("ConnectionDataBase") ;
-			
-			Connection=DriverManager.getConnection(ChaineConnection.split(",",-1)[0],ChaineConnection.split(",",-1)[1],ChaineConnection.split(",",-1)[2]);
-			
-	}
+
+    public static String CorpusPath;
+    public static String GlobalFiles;
+    public static String Abside;
+    public static String BlocMontant;
+    public static String CompteBloc;
+    public static String CompteClient;
+    public static String Operation;
+    public static String SERVEUR;
+    public static Integer PORT;
+    public static String USER;
+    public static String PASS;
+    public static String PathClient;
+    public static String PathCompteBloquer;
+    public static String PathMontantBloquer;
+    public static String PathMouvement;
+    public static String SERVEURSMTP;
+    public static String MAIL_USERNAME;
+    public static String MAIL_PASSWORD;
+    public static String FROM;
+    public static List<String> EmailAdmin;
+    public static Connection Connection;
+
+
+    public static void Init() throws IOException, SQLException, ClassNotFoundException {
+
+        Properties prop = new Properties();
+        FileInputStream in = new FileInputStream("Configuration.properties");
+        prop.load(in);
+        in.close();
+
+        Parametrage.CorpusPath = prop.getProperty("CorpusPath");
+        Parametrage.GlobalFiles = prop.getProperty("GlobalFiles");
+        Parametrage.Abside = prop.getProperty("Abside");
+        Parametrage.BlocMontant = prop.getProperty("BlocMontant");
+        Parametrage.CompteBloc = prop.getProperty("CompteBloc");
+        Parametrage.CompteClient = prop.getProperty("CompteClient");
+        Parametrage.Operation = prop.getProperty("Operation");
+        Parametrage.SERVEUR = prop.getProperty("SERVEUR");
+        Parametrage.PORT = Integer.parseInt(prop.getProperty("PORT"));
+        Parametrage.USER = prop.getProperty("USER");
+        Parametrage.PASS = prop.getProperty("PASS");
+        Parametrage.PathClient = prop.getProperty("PathClient");
+        Parametrage.PathCompteBloquer = prop.getProperty("PathCompteBloquer");
+        Parametrage.PathMontantBloquer = prop.getProperty("PathMontantBloquer");
+        Parametrage.PathMouvement = prop.getProperty("PathMouvement");
+        Parametrage.SERVEURSMTP = prop.getProperty("SERVEURSMTP");
+        Parametrage.MAIL_USERNAME = prop.getProperty("MAIL_USERNAME");
+        Parametrage.MAIL_PASSWORD = prop.getProperty("MAIL_PASSWORD");
+        Parametrage.FROM = prop.getProperty("FROM");
+        Parametrage.EmailAdmin = new ArrayList<String>();
+
+        String[] t = prop.getProperty("EmailAdmin").split(",", -1);
+        for (int i = 0; i < t.length; i++) {
+            Parametrage.EmailAdmin.add(t[i]);
+        }
+
+        Class.forName("com.mysql.jdbc.Driver");
+
+        String ChaineConnection = prop.getProperty("ConnectionDataBase");
+
+        Connection = DriverManager.getConnection(ChaineConnection.split(",", -1)[0], ChaineConnection.split(",", -1)[1], ChaineConnection.split(",", -1)[2]);
+
+    }
 
 }
--- src/loaddata/Mail.java
+++ src/loaddata/Mail.java
@@ -1,7 +1,6 @@
 package loaddata;
 
 
-
 import javax.mail.internet.*;
 
 import launch.Parametrage;
@@ -9,69 +8,71 @@ import launch.Traitement;
 
 import javax.mail.*;
 import java.util.*;
-/**
-* Classe permettant d'envoyer un mail.
-*/
-public class Mail {
-private final static String MAILER_VERSION = "Java";
 
-	public static boolean envoyerMailSMTP(String serveur, boolean debug) 
-	{
-		boolean result = false;
-		try 
-		{
-			Properties prop = System.getProperties();
-			prop.put("mail.smtp.host", serveur);
-			Session session = Session.getDefaultInstance(prop,null);
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress("redouane.boulbari@mf.gov.dz"));
-			InternetAddress[] internetAddresses = new InternetAddress[1];
-			internetAddresses[0] = new InternetAddress("abdenacer.djarrah@mf.gov.dz");
-			message.setRecipients(Message.RecipientType.TO,internetAddresses);
-			message.setSubject("Test");
-			message.setText("test mail");
-			message.setHeader("X-Mailer", MAILER_VERSION);
-			message.setSentDate(new Date());
-			//session.setDebug(debug);
-			Transport.send(message);
-			result = true;
-		} 
-		catch (AddressException e) 
-		{
-		e.printStackTrace();
-		}
-		catch (MessagingException e) 
-		{
-		e.printStackTrace();
-		}
-		
-		return result;
-	}
-	
-	public static void EnvoyerMail(launch.Message m) throws AddressException, MessagingException 
-	{
-		
-		Traitement.logger.info("*************************************BEGIN SEND MAIL*************************************");
-	      
-			Properties prop = System.getProperties();
-			prop.put("mail.smtp.host",Parametrage.SERVEURSMTP);
-			Session session = Session.getDefaultInstance(prop,null);
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(Parametrage.FROM));
-			InternetAddress[] internetAddresses = new InternetAddress[1];
-			internetAddresses[0] = new InternetAddress(m.getDESTINATAIRE());
-			message.setRecipients(Message.RecipientType.TO,internetAddresses);
-			message.setSubject("INFORMATION");
-			message.setText("BONJOUR Mr/Mm : "+m.getNom()+" "+m.getPrenom()+"\n"+m.getMsg());
-			message.setHeader("X-Mailer", MAILER_VERSION);
-			message.setSentDate(new Date());
-			
-			Transport.send(message);
-					
-			Traitement.logger.info("MAIL INFORMATION FOR :"+m.getDESTINATAIRE());
-	
-		Traitement.logger.info("*************************************END SEND MAIL*************************************");
-		      
-	}
-	
+/**
+ * Classe permettant d'envoyer un mail.
+ */
+public class Mail {
+    private final static String MAILER_VERSION = "Java";
+
+    public static boolean envoyerMailSMTP(String serveur, boolean debug) {
+        boolean result = false;
+        try {
+            Properties prop = System.getProperties();
+            prop.put("mail.smtp.host", serveur);
+            Session session = Session.getDefaultInstance(prop, null);
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("redouane.boulbari@mf.gov.dz"));
+            InternetAddress[] internetAddresses = new InternetAddress[1];
+            internetAddresses[0] = new InternetAddress("abdenacer.djarrah@mf.gov.dz");
+            message.setRecipients(Message.RecipientType.TO, internetAddresses);
+            message.setSubject("Test");
+            message.setText("test mail");
+            message.setHeader("X-Mailer", MAILER_VERSION);
+            message.setSentDate(new Date());
+            //session.setDebug(debug);
+            Transport.send(message);
+            result = true;
+        } catch (AddressException e) {
+            e.printStackTrace();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    public static void EnvoyerMail(launch.Message m) throws AddressException, MessagingException {
+
+        Traitement.logger.info("*************************************BEGIN SEND MAIL*************************************");
+
+        Properties prop = System.getProperties();
+        prop.put("mail.smtp.host", Parametrage.SERVEURSMTP);
+//			Session session = Session.getDefaultInstance(prop,null);
+        prop.put("mail.smtp.auth", "true"); // Enable authentication
+        prop.put("mail.smtp.port", "25");
+        prop.put("mail.smtp.starttls.enable", "false");
+        Session session = Session.getInstance(prop, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(Parametrage.MAIL_USERNAME, Parametrage.MAIL_PASSWORD);
+            }
+        });
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress(Parametrage.FROM));
+        InternetAddress[] internetAddresses = new InternetAddress[1];
+        internetAddresses[0] = new InternetAddress(m.getDESTINATAIRE());
+        message.setRecipients(Message.RecipientType.TO, internetAddresses);
+        message.setSubject("INFORMATION");
+        message.setText("BONJOUR Mr/Mm : " + m.getNom() + " " + m.getPrenom() + "\n" + m.getMsg());
+        message.setHeader("X-Mailer", MAILER_VERSION);
+        message.setSentDate(new Date());
+
+        Transport.send(message);
+
+        Traitement.logger.info("MAIL INFORMATION FOR :" + m.getDESTINATAIRE());
+
+        Traitement.logger.info("*************************************END SEND MAIL*************************************");
+
+    }
+
 }
