fa1b5f6aee8bb9846425761cb29dfde9ccb1107f
yassine1024 <atik.yassine.1024@gmail.com>
2023-06-07 12:35:58 +0200

    Change content message's mail and embed the send mail in try,catch clasue.

--- src/launch/Message.java
+++ src/launch/Message.java
@@ -2,38 +2,48 @@ package launch;
 
 public class Message {
 
-	private String DESTINATAIRE ;
-	private String Msg ;
-	private String nom ;
-	private String prenom ;
+    private String DESTINATAIRE;
+    private String Msg;
+    private String nom;
+    private String prenom;
 
-	
-	public String getDESTINATAIRE() {
-		return DESTINATAIRE;
-	}
-	public void setDESTINATAIRE(String dESTINATAIRE) {
-		DESTINATAIRE = dESTINATAIRE;
-	}
-	public String getMsg() {
-		return Msg;
-	}
-	public void setMsg(String msg) {
-		Msg = msg;
-	}
-	public String getNom() {
-		
-		return ((this.DESTINATAIRE.split("@",-1)[0]).split("\\.",-1)[1]).toUpperCase();
-	}
-	public String getPrenom() {
-		return ((this.DESTINATAIRE.split("@",-1)[0]).split("\\.",-1)[0]).toUpperCase();
-	}
-	@Override
-	public String toString() {
-		return "Message [DESTINATAIRE=" + DESTINATAIRE + ", Msg=" + Msg + "]";
-	}
-	
-	
-	
-	
-	
+
+    public String getDESTINATAIRE() {
+        return DESTINATAIRE;
+    }
+
+    public void setDESTINATAIRE(String dESTINATAIRE) {
+        DESTINATAIRE = dESTINATAIRE;
+    }
+
+    public String getMsg() {
+        return Msg;
+    }
+
+    public void setMsg(String msg) {
+        Msg = msg;
+    }
+
+    public String getNom() {
+
+        if ((this.DESTINATAIRE.split("@", -1)[0]).split("\\.", -1).length < 2) {
+            return this.DESTINATAIRE.split("@", -1)[0];
+        }
+        return ((this.DESTINATAIRE.split("@", -1)[0]).split("\\.", -1)[1]).toUpperCase();
+    }
+
+    public String getPrenom() {
+
+        if ((this.DESTINATAIRE.split("@", -1)[0]).split("\\.", -1).length < 2) {
+            return "";
+        }
+        return ((this.DESTINATAIRE.split("@", -1)[0]).split("\\.", -1)[0]).toUpperCase();
+    }
+
+    @Override
+    public String toString() {
+        return "Message [DESTINATAIRE=" + DESTINATAIRE + ", Msg=" + Msg + "]";
+    }
+
+
 }
--- src/loaddata/Mail.java
+++ src/loaddata/Mail.java
@@ -51,28 +51,56 @@ public class Mail {
 //			Session session = Session.getDefaultInstance(prop,null);
         prop.put("mail.smtp.auth", "true"); // Enable authentication
         prop.put("mail.smtp.port", "25");
-        prop.put("mail.smtp.starttls.enable", "false");
+        prop.put("mail.smtp.starttls.enable", "true");
         Session session = Session.getInstance(prop, new javax.mail.Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
                 return new PasswordAuthentication(Parametrage.MAIL_USERNAME, Parametrage.MAIL_PASSWORD);
             }
         });
+
+        try{
         Message message = new MimeMessage(session);
         message.setFrom(new InternetAddress(Parametrage.FROM));
         InternetAddress[] internetAddresses = new InternetAddress[1];
         internetAddresses[0] = new InternetAddress(m.getDESTINATAIRE());
         message.setRecipients(Message.RecipientType.TO, internetAddresses);
         message.setSubject("INFORMATION");
-        message.setText("BONJOUR Mr/Mm : " + m.getNom() + " " + m.getPrenom() + "\n" + m.getMsg());
-        message.setHeader("X-Mailer", MAILER_VERSION);
         message.setSentDate(new Date());
+
+            // Construct the email content
+            // Construct the email content
+            String emailContent = "Cher(e) " + m.getNom() + " " + m.getPrenom() +",\n\n" +
+                    "Nous espérons que vous allez bien. Nous souhaitons vous informer de l'état actuel de votre compte de trésorerie.\n\n" +
+                    "Selon nos enregistrements, " + m.getMsg() + " dinars algériens.\n\n" +
+                    "Veuillez noter que cette information est fournie à titre indicatif et peut être sujette à des changements en " +
+                    "fonction des transactions en cours.\n\n" +
+                    "Nous vous recommandons de consulter régulièrement votre compte de trésorerie et de prendre les mesures " +
+                    "nécessaires pour maintenir un solde adéquat.\n\n" +
+                    "Si vous avez des questions ou des préoccupations concernant votre compte de trésorerie, n'hésitez pas à " +
+                    "nous contacter à l'adresse " + "contact.dgtc@mf.gov.dz" + " ou au " + "0696273757" + ".\n\n" +
+                    "Nous vous remercions de votre confiance et de votre fidélité.\n\n" +
+                    "Cordialement,\n" +
+                    "DSI" + "\n" +
+                    "El_Racid" + "\n" +
+                    "Informations de contact : " + "contact.dgtc@mf.gov.dz" + " ou au " + "0696273757";
+            // Set the email content
+            message.setText(emailContent);
+
+
+//            message.setContent("BONJOUR Mr/Mm : " + m.getNom() + " " + m.getPrenom() + "\n" + m.getMsg(), "text/plain");
+//        message.setText("BONJOUR Mr/Mm : " + m.getNom() + " " + m.getPrenom() + "\n" + m.getMsg());
+//        message.setHeader("X-Mailer", MAILER_VERSION);
+
 
         Transport.send(message);
 
         Traitement.logger.info("MAIL INFORMATION FOR :" + m.getDESTINATAIRE());
 
         Traitement.logger.info("*************************************END SEND MAIL*************************************");
+        } catch (MessagingException e) {
 
+            Traitement.logger.warning("Failed to send email. Error: " + e.getMessage());
+        }
     }
 
 }
